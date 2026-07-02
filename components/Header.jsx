f24b66febbc73eacc935fedab01adc68f4e13c19
Albin Joby <albinjoby44@gmail.com>
2026-07-02 10:48:05 +0530

    Update IDEATOR header logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,24 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="py-4 md:py-6 text-primary bg-[#f5f5f5]">
       <div className="container  mx-auto flex justify-between items-center">
         <Link href="/">
-          <div className="flex">
-            <h1 className="lg:text-3xl text-xl font-semibold">IDEATOR.</h1>
-
-
-          </div>
-        </Link>
+  <div className="flex items-center">
+    <Image
+      src="/assests/idtr_logo_nw.png"
+      alt="IDEATOR"
+      width={220}
+      height={60}
+      priority
+      className="h-auto w-auto"
+    />
+  </div>
+</Link>
 
         <div className="hidden xl:flex">
           <Nav />
